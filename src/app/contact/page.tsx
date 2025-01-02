809290557a5192a2172752f3da63e1bd879f2aae
DawoodIqbal04 <dawoodjand356@gmail.com>
2025-01-02 16:34:49 +0500

    update commit

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import Header from "../components/header";
 import ShopInfo from "../components/ShopInfo";
 import { MdLocationPin } from "react-icons/md";
