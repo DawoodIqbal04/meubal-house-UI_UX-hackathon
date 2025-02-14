a1f216eabd6fdf10384a8fd37901cfb9c24c4846
DawoodIqbal04 <dawoodjand356@gmail.com>
2025-02-14 15:00:05 +0500

    update commit

--- src/app/components/CartProviders.tsx
+++ src/app/components/CartProviders.tsx
@@ -9,8 +9,8 @@ const CartProviders = ({ children }: { children: ReactNode }) => {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="http://localhost:3000//stripe/success"
-      cancelUrl="http://localhost:3000//stripe/error"
+      successUrl="http://meubal-house.vercel.app//stripe/success"
+      cancelUrl="http://meubal-house.vercel.app//stripe/error"
       currency="USD"
       billingAddressCollection={false}
       shouldPersist={true}
