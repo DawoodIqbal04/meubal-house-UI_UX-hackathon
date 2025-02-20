5f48756778909ac7d935a5fc3d6f455e50298ce0
DawoodIqbal04 <dawoodjand356@gmail.com>
2025-02-20 17:45:09 +0500

    dynamic commit

--- src/app/components/MenuModel.tsx
+++ src/app/components/MenuModel.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import React from "react";
-import { IoHeartOutline, IoSearchOutline } from "react-icons/io5";
 import {
   Sheet,
   SheetContent,
--- src/app/components/SingleProductBreadcrumb.tsx
+++ src/app/components/SingleProductBreadcrumb.tsx
@@ -1,33 +1,31 @@
-import React from 'react'
+import React from "react";
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-  } from "@/components/ui/breadcrumb"
-  
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
-const SPB = (props: {name: string}) => {
+const SPB = (props: { name: string }) => {
   return (
     <Breadcrumb>
-  <BreadcrumbList>
-    <BreadcrumbItem>
-      <BreadcrumbLink href="/">Home</BreadcrumbLink>
-    </BreadcrumbItem>
-    <BreadcrumbSeparator />
-    <BreadcrumbItem>
-      <BreadcrumbLink href="/shop">Shop</BreadcrumbLink>
-    </BreadcrumbItem>
-    <BreadcrumbSeparator />
-    <BreadcrumbItem>
-      <BreadcrumbPage>{props.name}</BreadcrumbPage>
-    </BreadcrumbItem>
-  </BreadcrumbList>
-</Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/shop">Shop</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>{props.name}</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
 
-  )
-}
-
-export default SPB
+export default SPB;
